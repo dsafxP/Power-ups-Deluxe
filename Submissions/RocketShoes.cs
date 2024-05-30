e51a99b317025096f1364ae2f3632dc6543e26e9
dsaf <ghostyn678@gmail.com>
2024-05-30 18:51:55 -0300

    Update RocketShoes

--- Submissions/RocketShoes.cs
+++ Submissions/RocketShoes.cs
@@ -1,6 +1,7 @@
 // ROCKET SHOES - Ebomb09
 public class RocketShoes : Powerup {
   private const uint EFFECT_COOLDOWN = 25;
+  private const float IMPULSE = 0.2f;
 
   private IObject[] _feet;
 
@@ -12,6 +13,26 @@ public class RocketShoes : Powerup {
         !Player.IsDiving &&
         !Player.IsGrabbing &&
         Player.IsInputEnabled;
+    }
+  }
+  
+  private bool Rocketing {
+    get {
+      return PlayerValid && Player.KeyPressed(VirtualKey.JUMP);
+    }
+  }
+  
+  private Vector2 Impulse {
+    get {
+      Vector2 impulse = new Vector2(0, Player.GetLinearVelocity().Y + IMPULSE);
+      
+      impulse.X += Player.KeyPressed(VirtualKey.AIM_RUN_RIGHT) ? 1 : 
+      (Player.KeyPressed(VirtualKey.AIM_RUN_LEFT) ? -1 : 0);
+      
+      impulse.X *= Player.KeyPressed(VirtualKey.SPRINT) ? 2 : 
+      (Player.KeyPressed(VirtualKey.WALKING) ? 0.5f : 1);
+      
+      return impulse;
     }
   }
 
@@ -42,41 +63,25 @@ public class RocketShoes : Powerup {
   }
 
   public override void Update(float dlt, float dltSecs) {
-    bool rocketing = PlayerValid && Player.KeyPressed(VirtualKey.JUMP);
-
-    if (rocketing) {
-      Vector2 impulse = new Vector2(0, Player.GetLinearVelocity().Y + 0.2f);
-
-      if (Player.KeyPressed(VirtualKey.AIM_RUN_RIGHT))
-        impulse.X += 1;
-
-      if (Player.KeyPressed(VirtualKey.AIM_RUN_LEFT))
-        impulse.X -= 1;
-
-      if (Player.KeyPressed(VirtualKey.SPRINT))
-        impulse.X *= 2;
-
-      if (Player.KeyPressed(VirtualKey.WALKING))
-        impulse.X /= 2;
-
-      Player.SetLinearVelocity(impulse);
-    }
+    Vector2 playerPos = Player.GetWorldPosition();
 
     foreach(IObject obj in _feet) {
       obj.SetLinearVelocity(Player.GetLinearVelocity());
 
       obj.SetAngle(
         Vector2Helper.AngleToPoint(
-          Player.GetWorldPosition(),
-          Player.GetWorldPosition() - new Vector2(Player.GetLinearVelocity().X, Math.Abs(Player.GetLinearVelocity().Y))
+          playerPos,
+          playerPos - new Vector2(Player.GetLinearVelocity().X, Math.Abs(Player.GetLinearVelocity().Y))
         )
       );
     }
 
-    _feet[0].SetWorldPosition(Player.GetWorldPosition() + new Vector2(-5, -2));
-    _feet[1].SetWorldPosition(Player.GetWorldPosition() + new Vector2(5, -2));
+    _feet[0].SetWorldPosition(playerPos + new Vector2(-5, -2));
+    _feet[1].SetWorldPosition(playerPos + new Vector2(5, -2));
 
-    if (rocketing) {
+    if (Rocketing) {
+      Player.SetLinearVelocity(Impulse);
+      
       if (Time % EFFECT_COOLDOWN == 0) {
         Game.PlayEffect("MZLED", Vector2.Zero, _feet[0].UniqueID, "MuzzleFlashS");
         Game.PlayEffect("MZLED", Vector2.Zero, _feet[1].UniqueID, "MuzzleFlashS");
