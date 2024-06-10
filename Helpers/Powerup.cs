d2884f008f7293651ac22301910fa0813ba798d9
dsaf <ghostyn678@gmail.com>
2024-06-09 21:19:45 -0300

    Revert "Update Powerup.cs"
    This reverts commit 7761364627178eac63539cb54009eb497abd695b.

--- Helpers/Powerup.cs
+++ Helpers/Powerup.cs
@@ -98,18 +98,24 @@ namespace PowerupsDeluxe {
       /// <param name="dlt">The time delta since the last update.</param>
       /// <param name="dltSecs">The time delta in seconds since the last
       /// update.</param>
-      public abstract void Update(float dlt, float dltSecs);
+      public virtual void Update(float dlt, float dltSecs) {
+        // Implement in derived classes
+      }
 
       /// <summary>
       /// Virtual method called when the power-up times out.
       /// </summary>
-      public abstract void TimeOut();
+      public virtual void TimeOut() {
+        // Implement in derived classes
+      }
 
       /// <summary>
       /// Virtual method called when the power-up is enabled or disabled. Called by
       /// the constructor.
       /// </summary>
-      public abstract void OnEnabled(bool enabled);
+      public virtual void OnEnabled(bool enabled) {
+        // Implement in derived classes
+      }
     }
   }
 }
